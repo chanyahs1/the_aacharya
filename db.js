37f1d3b865f89db7aa8f8e6fb9a15219ddab5139
chanyahs1 <yash.chandak.cd.che22@itbhu.ac.in>
2025-06-26 22:58:37 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'srv-db1872.hstgr.io',
+  host: process.env.DB_HOST || 'srv1872.hstgr.io',
   user: process.env.DB_USER || 'u393899762_emstheaacharya',
   password: process.env.DB_PASSWORD || 'Yash0770@',
   database: process.env.DB_NAME || 'u393899762_emstheaacharya',
