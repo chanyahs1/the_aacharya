25ff0242ed57a38f1621da054dfe1562c926c137
chanyahs1 <yash.chandak.cd.che22@itbhu.ac.in>
2025-06-27 01:03:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -222,6 +222,11 @@ app.use("/api/employees/lol", lol); // Test route, can be removed later
 app.use("/api/login-logs", loginLogsRoutes);
 app.use("/api/salary", salaryRoutes);
 
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
